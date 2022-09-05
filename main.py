afb87f7ed8cabdde939d0e597c68b6b004964fba
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-09-05 13:11:32 +0530

    .py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 import os
 
 # Get the API key from the environment variable
-API_KEY = os.environ.get('e544a9cda75646aab4ac34dab997c6ed')
+API_KEY = os.environ.get('Your-API-Key')
 
 # Get the news from the API
 def get_news():
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey=e544a9cda75646aab4ac34dab997c6ed'
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey=Your-API-Key'
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
